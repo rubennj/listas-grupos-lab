457eb8c07200778a50c94ab101956d03d0534bc5
rubennj <rubennj@users.noreply.github.com>
2021-06-11 08:43:10 +0200

    Update lee_grupos.py

--- lee_grupos.py
+++ lee_grupos.py
@@ -9,6 +9,7 @@ from math import ceil, floor
 from itertools import product
 import pandas as pd
 from pathlib import Path
+import random
 
 PATH_LISTAS = Path('listas_apolo')
 
@@ -25,7 +26,7 @@ grados = pd.DataFrame({'nombre': ['A302', 'A309', 'EE309'],
 # asignatura = asignaturas.loc[0]
 l = []
 lista_total = []
-lista_estudiantes_subgrupos = pd.DataFrame()
+lista_estudiantes_subgrupos = pd.DataFrame()                                                                
 for _, asignatura in asignaturas.iterrows():
     lista_todos_grados = []
     for _, grado in grados.iterrows():
@@ -40,8 +41,8 @@ for _, asignatura in asignaturas.iterrows():
     
     estudiantes_asignatura = pd.concat(lista_todos_grados)
     
-    # genera en la lista de estudiantes de la asignatura columna con el subgrupo
-    estudiantes_asignatura[f'subgrupo_{asignatura["nombre"]}'] = None
+    # genera e inicializa a 0 en la lista de estudiantes de la asignatura columna con el subgrupo
+    estudiantes_asignatura[f'subgrupo_{asignatura["nombre"]}'] = '-'
     
     num_grupos_asignatura = int(ceil(len(estudiantes_asignatura) / asignatura['plazas_sesion']))
     num_subgrupos_dia = int(floor(num_grupos_asignatura / len(asignatura['horario_sesiones'])))
@@ -55,44 +56,44 @@ for _, asignatura in asignaturas.iterrows():
     # baraja estudiantes
     estudiantes_asignatura = estudiantes_asignatura.sample(frac=1, random_state=123)
     
-    # lista ordenando primero los estudiantes con restricciones
+    # lista ordenando primero los estudiantes con restricciones para priorizar el reparto
     lista_estudiantes_asignatura = estudiantes_asignatura.sort_values(by=['limitaciones_grado'])
     
     for idx_correo, estudiante in lista_estudiantes_asignatura.iterrows():
+        # aleatoriza la lista de subgrupos para cada estudiante que se asigna
+        random.seed(123)
+        random.shuffle(lista_subgrupos_asignatura)
         for subgrupo_a_asignar in lista_subgrupos_asignatura:
             subgrupos_tamaños = lista_estudiantes_asignatura.groupby(f'subgrupo_{asignatura["nombre"]}').size()
             # cuenta plazas de cada subgrupo. La primera vez está vacío, por lo que se comprueba
             if subgrupo_a_asignar not in subgrupos_tamaños or subgrupos_tamaños[subgrupo_a_asignar] < asignatura['plazas_sesion']:
                 sesion_grupo_a_asignar = subgrupo_a_asignar.split('-')[0]
-                # comprueba si la sesión ya ha sido asignada al estudiante anteriormente en otra asignatura
+                # si el estudiante ya tiene un subgrupo de una asignación previa, se obtiene
                 if idx_correo in lista_estudiantes_subgrupos.index:
-                    sesiones_ya_asignadas = [sesion.split('-')[0] for sesion in lista_estudiantes_subgrupos.loc[idx_correo].values if isinstance(sesion, str)]
+                    sesiones_ya_asignadas = [subgrupo.split('-')[0] for subgrupo in lista_estudiantes_subgrupos.loc[idx_correo] if subgrupo != '-']
                     print('Sesiones YA asignadas', sesiones_ya_asignadas)
-                    # if idx_correo == 'd.hsanchez@alumnos.upm.es':
-                        # raise SystemError
-                    if sesion_grupo_a_asignar not in sesiones_ya_asignadas:
+                else:
+                    sesiones_ya_asignadas = []
+                if sesion_grupo_a_asignar not in sesiones_ya_asignadas:
+                    # estudiante sin restricciones
+                    if estudiante['limitaciones_grado'] is None:
+                        print(asignatura["nombre"], idx_correo, subgrupo_a_asignar)
                         lista_estudiantes_asignatura.at[idx_correo, f'subgrupo_{asignatura["nombre"]}'] = subgrupo_a_asignar
                         break
-                    else:
-                    # si ya la sesión ya ha sido asignada en otra asignatura, sale y busca en otro grupo
-                        continue
-                # estudiante sin restricciones
-                if estudiante['limitaciones_grado'] is None:
-                    print(asignatura["nombre"], idx_correo, subgrupo_a_asignar)
-                    lista_estudiantes_asignatura.at[idx_correo, f'subgrupo_{asignatura["nombre"]}'] = subgrupo_a_asignar
-                    break
-                # estudiante con restricciones, se mira cual es su grupo horario. Si es compatible lo coge
-                elif sesion_grupo_a_asignar in estudiante['limitaciones_grado']:
-                    print(asignatura["nombre"], idx_correo, subgrupo_a_asignar)
-                    lista_estudiantes_asignatura.at[idx_correo, f'subgrupo_{asignatura["nombre"]}'] = subgrupo_a_asignar
-                    break
-                # si el subgrupo no es compatible no lo asigna y sigue buscando
-
+                    # estudiante con restricciones, se mira cual es su grupo horario. Si es compatible lo coge
+                    elif sesion_grupo_a_asignar in estudiante['limitaciones_grado']:
+                        print(asignatura["nombre"], idx_correo, subgrupo_a_asignar)
+                        lista_estudiantes_asignatura.at[idx_correo, f'subgrupo_{asignatura["nombre"]}'] = subgrupo_a_asignar
+                        break
+                    # si el subgrupo no es compatible no lo asigna y sigue buscando
+                else:
+                # si ya la sesión ya ha sido asignada en otra asignatura, sale y busca en otro grupo
+                    continue
             else:
                 print(asignatura["nombre"], idx_correo, 'No hay plazas en el grupo', subgrupo_a_asignar)
  
             # comprueba si no consigue asignar subgrupo
-            # if subgrupo_a_asignar == lista_subgrupos_asignatura[-1]:
+            # if subgrupo_a_asignar == lista_subgrupos_asignatura[-1]:No docum
                 # raise SystemError('No hay subgrupo disponible, los siguientes estudiantes no están asignados:', estudiantes_asignatura.loc[idx_correo:])
     
     lista_total.append(lista_estudiantes_asignatura)
