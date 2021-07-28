59e0da413f228df1de7fa66245ea106934f60a13
rubennj <rubennj@users.noreply.github.com>
2021-07-28 20:05:25 +0200

    corrige calculo semanas de subgrupos
    Falta por rematar el asignar subgrupo en caso de misma sesion y semana (ver como hacer 2x break o refactorizar)

--- lee_grupos.py
+++ lee_grupos.py
@@ -28,7 +28,7 @@ asignaturas = pd.DataFrame(index=['instrumentacion', 'potencia', 'robotica'],
                                  'num_sesiones': [3, 2, 3],
                                  'horario_sesiones': [['MI09', 'MI11', 'JU09', 'JU11', 'VI11'], ['MI11', 'JU11', 'VI09'], ['MA11', 'MI09', 'MI11']],
                                  'num_subgrupos': [4, 7, 3],
-                                 'semana_inicial': [3, 1, 3]
+                                 'semana_inicial': [3, 1, 3],
                                  })
 
 grupos_grado = pd.DataFrame(index=['A302', 'A309', 'EE309'],
@@ -38,14 +38,21 @@ grupos_grado = pd.DataFrame(index=['A302', 'A309', 'EE309'],
 
 
 def semanas_subgrupo(nombre_asignatura, subgrupo):
-    # nombre_asignatura = 'potencia'
+    # nombre_asignatura = 'instrumentacion'
     # subgrupo = 'MA11-C'
-    
+    # semana_inicial = 1
+    # num_sesiones = 3
+    # num_subgrupos = 4
+
     semana_inicial = asignaturas.loc[nombre_asignatura, 'semana_inicial']
-    semana_subgrupo = ord(subgrupo[-1]) - 65
+    offset_subgrupo = ord(subgrupo[-1]) - 65
     num_sesiones = asignaturas.loc[nombre_asignatura, 'num_sesiones']
-    
-    semanas_sesiones = [sem for sem in range(num_sesiones) + semana_inicial + semana_subgrupo]
+    num_subgrupos = asignaturas.loc[nombre_asignatura, 'num_subgrupos']
+
+    offset = semana_inicial+offset_subgrupo
+
+    semanas_sesiones = [sem for sem in list(
+        range(offset, num_subgrupos*num_sesiones+offset, num_subgrupos))]
 
     return semanas_sesiones
 
@@ -78,7 +85,7 @@ for _, asignatura in asignaturas.iterrows():
         asignatura['horario_sesiones'], string.ascii_uppercase[:num_subgrupos_sesion])))
 
     ciclo_subgrupos_asignatura = deque(lista_subgrupos_asignatura)
-    
+
     # baraja estudiantes
     estudiantes_asignatura = estudiantes_asignatura.sample(
         frac=1, random_state=SEMILLA_RND)
@@ -87,14 +94,13 @@ for _, asignatura in asignaturas.iterrows():
     lista_estudiantes_asignatura = estudiantes_asignatura.sort_values(
         by=['prioridad_reparto_grupo_grado'])
 
-    n = 0
     for idx_correo, estudiante in lista_estudiantes_asignatura.iterrows():
         logging.info('\n\nEstudiante: %s , grupo: %s', idx_correo,
                      estudiante['Grupo matrícula'][-6:-1])
         # aleatoriza la lista de subgrupos para cada estudiante que se asigna
         # random.seed(SEMILLA_RND)
         # random.shuffle(lista_subgrupos_asignatura)
-        if idx_correo == 'jorge.devivar.adrada@alumnos.upm.es':
+        if idx_correo == 'christian.clemente.defrutos@alumnos.upm.es':
             print('asdf')
 
         for subgrupo_a_asignar in ciclo_subgrupos_asignatura:
@@ -102,11 +108,11 @@ for _, asignatura in asignaturas.iterrows():
                 f'subgrupo_{asignatura.name}').size()
             # cuenta plazas de cada subgrupo. La primera vez está vacío, por lo que se comprueba
             if subgrupo_a_asignar not in subgrupos_tamaños or subgrupos_tamaños[subgrupo_a_asignar] < asignatura['plazas_sesion']:
-                sesion_grupo_a_asignar = subgrupo_a_asignar.split('-')[0]
+                sesion_subgrupo_a_asignar = subgrupo_a_asignar.split('-')[0]
                 # si el estudiante ya tiene un subgrupo de una asignación previa, se obtiene
                 if idx_correo in lista_estudiantes_subgrupos.index:
                     subgrupos_ya_asignados = {
-                        subgrupo:asignatura_subgrupo for asignatura_subgrupo, subgrupo in lista_estudiantes_subgrupos.loc[idx_correo].items() if subgrupo != '-' and pd.notna(subgrupo)}
+                        subgrupo: asignatura_subgrupo for asignatura_subgrupo, subgrupo in lista_estudiantes_subgrupos.loc[idx_correo].items() if subgrupo != '-' and pd.notna(subgrupo)}
                     logging.info('Asignatura %s - sesiones previamente asignadas %s',
                                  asignatura.name, subgrupos_ya_asignados)
                     sesiones_subgrupos_ya_asignados = [sesion.split(
@@ -114,20 +120,28 @@ for _, asignatura in asignaturas.iterrows():
                 else:
                     subgrupos_ya_asignados = []
                     sesiones_subgrupos_ya_asignados = []
-                    
+
                 # si el subgrupo coincide (sesión+subgrupo) hay que comprobar que no coincidan las semanas
-                if subgrupo_a_asignar in subgrupos_ya_asignados:
-                    asignatura_subgrupo_asignado = subgrupos_ya_asignados[subgrupo_a_asignar].split('_')[1]
-                    semanas_subgrupos_ya_asignados = semanas_subgrupo(asignatura_subgrupo_asignado, subgrupo_a_asignar)
-                    semanas_subgrupos_a_asignar = semanas_subgrupo(asignatura.name, subgrupo_a_asignar)
-                    if set(semanas_subgrupos_ya_asignados).intersection(set(semanas_subgrupos_a_asignar)):
-                        logging.error(
-                            '%s no consigue asignar el subgrupo %s. Coinciden las semanas del subgrupo con otras anteriormente asignadas', asignatura.name, subgrupo_a_asignar)
-                        continue
- 
-                lista_subgrupos_ya_asignados = [subgrupo for subgrupo in subgrupos_ya_asignados]
-                
-                if subgrupo_a_asignar not in lista_subgrupos_ya_asignados:
+                # lista_subgrupos_ya_asignados = [subgrupo for subgrupo in subgrupos_ya_asignados]
+
+                if sesion_subgrupo_a_asignar in sesiones_subgrupos_ya_asignados:
+                    for subgrupo in subgrupos_ya_asignados:
+                        asignatura_subgrupo_asignado = subgrupos_ya_asignados[subgrupo].split('_')[1]
+                        semanas_subgrupos_ya_asignados = semanas_subgrupo(
+                            asignatura_subgrupo_asignado, subgrupo_a_asignar)
+                        semanas_subgrupos_a_asignar = semanas_subgrupo(
+                            asignatura.name, subgrupo_a_asignar)
+                        if any(item in semanas_subgrupos_ya_asignados for item in semanas_subgrupos_a_asignar):
+                            logging.error(
+                                '%s no consigue asignar el subgrupo %s con semanas %s. Coinciden alguna semana con las del subgrupo %s de %s: %s', asignatura.name, subgrupo_a_asignar, semanas_subgrupos_a_asignar, subgrupo, asignatura_subgrupo_asignado, semanas_subgrupos_ya_asignados)
+                        else:
+                            logging.info('%s asignado a %s (semanas %s)',
+                                         asignatura.name, subgrupo_a_asignar, semanas_subgrupos_a_asignar)
+                            lista_estudiantes_asignatura.at[idx_correo,
+                                                        f'subgrupo_{asignatura.name}'] = subgrupo_a_asignar
+                            break # refactorizar. Faltaría un doble break!
+
+                else:
                     # estudiante sin restricciones
                     if estudiante['limitaciones_grupo_grado'] is None:
                         logging.info('%s asignado a %s',
@@ -136,7 +150,7 @@ for _, asignatura in asignaturas.iterrows():
                                                         f'subgrupo_{asignatura.name}'] = subgrupo_a_asignar
                         break
                     # estudiante con restricciones, se mira cual es su grupo horario. Si es compatible lo coge
-                    elif sesion_grupo_a_asignar in estudiante['limitaciones_grupo_grado'][f'{asignatura.name}']:
+                    elif sesion_subgrupo_a_asignar in estudiante['limitaciones_grupo_grado'][f'{asignatura.name}']:
                         logging.info('%s asignado a %s',
                                      asignatura.name, subgrupo_a_asignar)
                         lista_estudiantes_asignatura.at[idx_correo,
@@ -147,9 +161,6 @@ for _, asignatura in asignaturas.iterrows():
                             '%s no consigue asignar el subgrupo %s', asignatura.name, subgrupo_a_asignar)
                         continue
                     # si el subgrupo no es compatible, sale y busca en otro grupo
-                # else:
-                    # Nunca llega a este else, ya que los 2 ifs anteriores cubren todas las posibilidades
-                    # continue
             else:
                 logging.info('No hay plazas en el grupo %s',
                              subgrupo_a_asignar)
@@ -158,14 +169,15 @@ for _, asignatura in asignaturas.iterrows():
             if subgrupo_a_asignar == ciclo_subgrupos_asignatura[-1]:
                 logging.error(
                     'Asignatura %s No hay subgrupo disponible para %s', asignatura.name, idx_correo)
-        ciclo_subgrupos_asignatura.rotate(1) 
+        ciclo_subgrupos_asignatura.rotate(1)
 
     logging.error('\n\nLista estudiantes sin grupo de %s', asignatura.name)
     logging.error(
         lista_estudiantes_asignatura[lista_estudiantes_asignatura[f'subgrupo_{asignatura.name}'] == '-'])
-    
-    logging.info(lista_estudiantes_asignatura.groupby(by=f'subgrupo_{asignatura.name}').size())
-    
+
+    logging.info(lista_estudiantes_asignatura.groupby(
+        by=f'subgrupo_{asignatura.name}').size())
+
     # lista_total.append(lista_estudiantes_asignatura)
     l.append(lista_estudiantes_asignatura[f'subgrupo_{asignatura.name}'])
     lista_estudiantes_subgrupos = pd.concat(l, axis=1)
