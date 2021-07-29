76ccb2dd2051512a4c24873ddbaa918d731ecbd0
rubennj <rubennj@users.noreply.github.com>
2021-07-29 14:41:41 +0200

    entre parentesis, para hacerlo evidente

--- lee_grupos.py
+++ lee_grupos.py
@@ -145,7 +145,7 @@ for _, asignatura in asignaturas.iterrows():
                     # estudiante sin restricciones
                     cond_sin_restr = estudiante['limitaciones_grupo_grado'] is None
                     # estudiante con restricciones, se mira si no está vacía la lista de limitaciones y cual es su grupo horario. Si es compatible lo coge
-                    cond_con_restr = estudiante['limitaciones_grupo_grado'] and sesion_subgrupo_a_asignar in estudiante['limitaciones_grupo_grado'][f'{asignatura.name}']
+                    cond_con_restr = estudiante['limitaciones_grupo_grado'] and (sesion_subgrupo_a_asignar in estudiante['limitaciones_grupo_grado'][f'{asignatura.name}'])
                     if cond_sin_restr or cond_con_restr:
                         logging.info('%s asignado a %s',
                                      asignatura.name, subgrupo_a_asignar)
