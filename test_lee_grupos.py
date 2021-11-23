59b690876e0cb9859e36fe5026f5f4336a6234c6
AndreaMagro7 <andreamagrocns@gmail.com>
2021-11-23 01:03:02 +0100

    Añadido test
    Se ha realizado un test para comprobar que asignaturas van bien y cuáles no.

--- test_lee_grupos.py
+++ test_lee_grupos.py
@@ -7,5 +7,18 @@ Created on Fri Jul 30 17:56:26 2021
 import pandas as pd
 from pathlib import Path
 
-lista_subgrupos = pd.read_excel('lista_subgrupos.xlsx', index_col='Email').sort_index()
+lista_subgrupos = pd.read_excel('lista_subgrupos.xlsx', dtype={'Nº Expediente en Centro':str})
+lista_subgrupos.set_index('Nº Expediente en Centro', inplace=True)
 
+asignaturas = ['instrumentacion', 'potencia', 'robotica', 'infind', 'automatizacion']
+asignaturas_plazas = [165-160, 167-168, 147-180, 208-250, 189-240]
+
+for index in range(len(asignaturas)):
+    aux = lista_subgrupos.loc[lista_subgrupos[f'subgrupo_{asignaturas[index]}'] == '-']
+    if len(aux) > (asignaturas_plazas[index] if asignaturas_plazas[index] > 0 else 0):
+        print(aux)
+        print(f'{asignaturas[index]} esta mal.')
+    else:
+        if len(aux) != 0:
+            print(aux)
+        print(f'{asignaturas[index]} esta bien.')
